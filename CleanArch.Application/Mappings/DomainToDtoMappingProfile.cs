b9b75b15bf737214ac388d29d237164745d0f022
Felipe Estevam Luiz <fel13.luiz@gmail.com>
2022-08-10 21:13:04 -0300

    criando e mapeando os DTOs

---
+++ CleanArch.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.Mappings
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
