f2dc3a61edfaf2733863c9504c3bb4d4318099c4
Felipe Estevam Luiz <fel13.luiz@gmail.com>
2022-08-15 22:18:45 -0300

    create commands and querys

---
+++ CleanArch.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domain.Entities;
+using MediatR;
+
+namespace CleanArch.Application.Products.Commands
+{
+    public struct ProductRemoveCommand : IRequest<Product>
+    {
+        public int Id { get; private set; }
+
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ CleanArch.Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace CleanArch.Application.Products.Queries
+{
+    public struct GetProductsQuery : IRequest<IEnumerable<Product>>
+    {
+    }
+}
